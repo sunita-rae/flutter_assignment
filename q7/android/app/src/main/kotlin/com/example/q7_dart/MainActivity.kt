5afe7fa226d829b6f69b7cf3bda9015c121cb8ce
sunita rai <sunita.rae05@gmail.com>
2026-07-28 07:36:20 +0545

    first commit

---
+++ q7/android/app/src/main/kotlin/com/example/q7_dart/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.q7_dart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ q8/android/app/src/main/kotlin/com/example/q8_dart/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.q8_dart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
